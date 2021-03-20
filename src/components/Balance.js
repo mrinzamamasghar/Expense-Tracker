46c91e50a9452d425dcec22e1e0e8fe5321e8959
inzamam asghar <mrinzamamasghar>
2021-03-20 19:44:05 +0500

    console addede

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -8,7 +8,7 @@ function Balance(props) {
 
   const amount = transactions.map((amount) => amount.amount);
   const sum = amount.reduce((acc, itm) => (acc += itm), 0).toFixed(2);
-
+  console.log("new testing console....");
   return (
     <>
       <h4>Your Balance</h4>
